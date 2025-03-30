502fcaefa1a512ead3a603c3fb42dad3122287fd
cjc545 <cal.j.clarke@live.com>
2025-03-30 13:48:55 +0100

    api slightly implemented

--- src/app.js
+++
@@ -1,7 +0,0 @@
-const sqlite = require('sqlite3').verbose()
-const bodyParser = require()
-
-const db = new sqlite.Database(`./user.db`, sqlite.OPEN_READWRITE, (err)=>{
-    if(err) return console.error(err);
-})
-
